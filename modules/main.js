d721327368081846869c6706bf88e4f2d47ce036
flowerkorr <d7983029613@daum.net>
2024-01-19 21:53:51 +0900

    dddd

--- modules/main.js
+++ modules/main.js
@@ -5,6 +5,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'templates');
 
 app.use(require('../setting'));
+
+// app.use('/user', require('./user/md'));
 app.use('/user', require('./user/get'));
 app.use('/user', require('./user/post'));
 app.use('/user', require('./user/put'));
