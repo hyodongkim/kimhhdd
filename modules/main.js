066df8ea1210bfe52a753b14858a62f39a06e2d8
flowerkorr <d7983029613@daum.net>
2024-01-22 21:44:22 +0900

    eee

--- modules/main.js
+++ modules/main.js
@@ -1,14 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express({xPoweredBy:false});
+const upload = require('./file');
 
 app.set('view engine', 'ejs');
 app.set('views', 'templates');
 
-app.use(require('../setting'));
+app.use(require('./setting'));
 
-// app.use('/user', require('./user/md'));
-app.use('/user', require('./user/get'));
-app.use('/user', require('./user/post'));
-app.use('/user', require('./user/put'));
+// 미들웨어에서 제어 -> 파일 다운로드 제한
+
+app.post('/upload/single', upload.single("file"), (req,res,next)=>{
+    console.log(req.file);
+    res.redirect("/");
+});
+app.post('/upload/array', upload.array("file", 5), (req,res,next)=>{
+    console.log(req.files);
+    res.redirect("/");
+});
+app.post('/upload/fields', upload.fields([{name:"file",maxCount:5}]), (req,res,next)=>{
+    console.log(req.files);
+    res.redirect("/");
+});
+app.post('/upload/none', upload.none(), (req,res,next)=>{
+    res.redirect("/");
+});
+app.post('/upload/any', upload.any(), (req,res,next)=>{
+    console.log(req.files);
+    res.redirect("/");
+});
+
+app.get('/uploads', (req,res,next)=>{
+    res.render('uploads');
+})
+
+app.post((req,res,next)=>{
+    res.send("test ok");
+});
 
 module.exports = app;
---
+++ modules/setting.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+require('dotenv').config({path:'config/.env'});
+router.use(require('cookie-parser')(process.env.COOKIE_SECRET));
+router.use((req,res,next)=>{
+    res.setCookie = (key, value, age)=>{
+        res.cookie(key, value, {
+            path:'/',
+            domain:req.domain,
+            secure: false,
+            httpOnly: false,
+            signed: true,
+            maxAge: age
+        });
+    };
+    res.removeCookie = (key)=>{
+        res.clearCookie(key,  {
+            path:'/',
+            domain:req.domain,
+            secure: false,
+            httpOnly: false,
+            signed: true
+        });
+    };
+    res.removeAllCookie = ()=>{
+        for(let key in req.signedCookies){
+            res.clearCookie(key, {
+                path:'/',
+                domain:req.domain,
+                secure: false,
+                httpOnly: false,
+                signed: true
+            });
+        }
+    };
+    req.totalCookies = {...req.cookies, ...req.signedCookies};
+    next();
+});
+router.use(require('cors')({
+    origin:`https://localhost:${process.env.PORT}`,
+    methods:['get','post'],
+    allowedHeaders:['Content-Type'],
+    exposedHeaders:['Content-Type'],
+    credentials:true,
+    maxAge: 1000 * 60 * 30
+}));
+router.use(express.json());
+router.use(express.raw());
+router.use(express.text());
+router.use(express.urlencoded({extended:true}));
+router.use('/resources', express.static('resources', {
+    dotfiles:"ignore",
+    extensions:[],
+    fallthrough:true,
+    immutable:false,
+    maxAge:18000000,
+    index:false,
+    redirect:false
+}));
+
+module.exports = router;
